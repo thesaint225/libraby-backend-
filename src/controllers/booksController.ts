b9c715db69c63ea0d1f40c81d1e51ed9725a9631
henrymiessan <miessanhenri@gmail.com>
2025-03-09 23:56:55 +0000

    add return statements to prevent further execution in controllers

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,125 +1,86 @@
 import { Request, Response, NextFunction } from "express";
 import Book from "../models/booksModel";
-import { MongoServerError } from "mongodb";
+import { asyncHandler } from "../helpers/asyncHandler";
 
 // @description show all books
 // @routes api/v1/books
 // @acess  public
 
-export const getBooks = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const books = await Book.find({});
-    res.status(200).json({
-      success: true,
-      msg: "show all books",
-      data: books,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+export const getBooks = asyncHandler(async (req: Request, res: Response) => {
+  const books = await Book.find({});
+  return res.status(200).json({
+    success: true,
+    msg: "show all books",
+    data: books,
+  });
+});
 
 // description show a single book
 // routes api/v1/books
 // @access public
 
-export const getBook = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const book = await Book.findById(id);
-    if (!book) {
-      res.status(404).json({
-        success: false,
-        msg: "book not found",
-      });
-    }
-    res.status(200).json({ success: true, msg: `show book ${id}` });
-  } catch (error) {
-    next(error);
+export const getBook = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const book = await Book.findById(id);
+  if (!book) {
+    return res.status(404).json({
+      success: false,
+      msg: "book not found",
+    });
   }
-};
+  res.status(200).json({ success: true, msg: `show book ${id}`, data: book });
+});
 
 // @description create Bootcamp
 // route api/v1/books
 // @access public
-export const createBook = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const book = await Book.create(req.body);
-    res.status(201).json({
-      success: true,
-      msg: "book created ",
-      data: book,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+export const createBook = asyncHandler(async (req: Request, res: Response) => {
+  const book = await Book.create(req.body);
+  return res.status(201).json({
+    success: true,
+    msg: "book created ",
+    data: book,
+  });
+});
 
 // description update single book
 // route api/v1/books
 // @access public
-export const updateBook = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const updateBook = await Book.findByIdAndUpdate(id, req.body, {
-      // return the updated book
-      new: true,
-      // Ensure update folow  schemaValidation
-      runValidators: true,
+export const updateBook = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updateBook = await Book.findByIdAndUpdate(id, req.body, {
+    // return the updated book
+    new: true,
+    // Ensure update folow  schemaValidation
+    runValidators: true,
+  });
+  if (!updateBook) {
+    return res.status(404).json({
+      success: false,
+      msg: "book not found ",
     });
-    if (!updateBook) {
-      res.status(404).json({
-        success: false,
-        msg: "book not found ",
-      });
-    }
-    res
-      .status(200)
-      .json({ success: true, msg: `update book ${id}`, data: updateBook });
-  } catch (error) {
-    next(error);
   }
-};
+  res
+    .status(200)
+    .json({ success: true, msg: `update book ${id}`, data: updateBook });
+});
 
 // @description delete single book
 // @route Delete api/v1/books
 // @access public
 
-export const deleteBook = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const deleteBook = await Book.findByIdAndDelete(id);
-    if (!deleteBook) {
-      res.status(404).json({
-        success: false,
-        msg: "book not found",
-      });
-    }
-
-    res
-      .status(200)
-      .json({ success: true, msg: `delete book ${id}`, data: deleteBook });
-  } catch (error) {
-    next(error);
+export const deleteBook = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const deleteBook = await Book.findByIdAndDelete(id);
+  if (!deleteBook) {
+    res.status(404).json({
+      success: false,
+      msg: "book not found",
+    });
+    return;
   }
-};
+
+  res
+    .status(200)
+    .json({ success: true, msg: `delete book ${id}`, data: deleteBook });
+});
